479775b284a870e24cb36f953821784f98ea9879
Naman Agrawal <na2603@columbia.edu>
2018-02-14 03:59:15 -0500

    Updated NYT2 visualization

--- A3/nyttext2.js
+++ A3/nyttext2.js
@@ -21,24 +21,33 @@ function setup() {
 
 function draw() {
   background(0);
-  fill(255);
-  text("Topics of Interest", 0, 0);
+  translate(40, 40);
 
-  var lineheight = 24;
-  var margin = 40;
-  translate(margin, margin);
+  textSize(20);
+  fill(255);
+  text("Political Colors: highlighting U.S. politics keywords in NYT's top story abstracts", 0, 0);
+  fill(230, 230, 230, 240);
+  textSize(14);
+
+  text("(Trump administration in yellow, Republicans in red, Democrats in blue)", 0, 24);
 
   for (var i = 0; i < headlines.length; i++) {
     var words = split(headlines[i], ' ');
     var nextX = 0;
 
     for (var j = 0; j < words.length; j++) {
-      if (words[j].toLowerCase().indexOf('trump') === -1) {
-        fill(255);
+      if (words[j].toLowerCase().indexOf('trump') !== -1 ||
+        (words[j].toLowerCase().indexOf('house') !== -1 && words[j-1].toLowerCase() === 'white')) {
+        fill(255, 235, 39, 240);
+      } else if (words[j].toLowerCase().indexOf('g.o.p.') !== -1 ||
+        words[j].toLowerCase().indexOf('republican') !== -1) {
+        fill(255, 82, 82, 240);
+      } else if (words[j].toLowerCase().indexOf('democrat') !== -1) {
+        fill(130, 177, 255, 240);
       } else {
-        fill("yellow");
+        fill(255, 255, 255, 240);
       }
-      text(words[j] +' ', nextX, i*lineheight);
+      text(words[j] +' ', nextX, (i+2.5)*24);
       nextX += textWidth(words[j]+' ');
     }
   }
